6e0060a16c3d343a333941666ccd7d09937cd002
UchithmaSenevirathne <uchithma@gmail.com>
2024-05-28 22:27:35 +0530

    create sidebar

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Sidebar from './Sidebar'
 
 export default function Layout() {
     return(
-        <div>
-            <div className='bg-sky-200'>sidebar</div>
-            <div className='bg-teal-200'>header</div>
-            <div>{<Outlet/>}</div>
-            <p>footer</p>
+        <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
+            <Sidebar/>
+            <div className='p-4'>
+                <div className='bg-teal-200'>header</div>
+                <div>{<Outlet/>}</div>
+            </div>
         </div>
     )
 }
