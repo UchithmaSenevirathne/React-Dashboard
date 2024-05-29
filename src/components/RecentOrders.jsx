44233f3f17a4277949821106d9f333d7f7f61dd3
UchithmaSenevirathne <uchithma@gmail.com>
2024-05-29 17:42:28 +0530

    add some border to recent orders table

--- src/components/RecentOrders.jsx
+++ src/components/RecentOrders.jsx
@@ -70,7 +70,7 @@ function RecentOrders() {
         <div className='bg-white px-4 pt-3 pb-4 rounded-sm border border-gray-200 flex-1'>
             <strong className='text-gray-700 font-medium'>Recent Orders</strong>
             <div className='mt-3'>
-                <table className='w-full text-gray-700'>
+                <table className='w-full text-gray-700 border-x border-gray-200 rounded-sm'>
                     <thead>
                     <tr>
                         <td>ID</td>
