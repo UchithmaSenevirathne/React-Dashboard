d49eebdf84326f13716729f2cc549fa017f93918
UchithmaSenevirathne <uchithma@gmail.com>
2024-05-29 10:12:36 +0530

    create header

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
+import Header from './Header'
 
 export default function Layout() {
     return(
         <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
             <Sidebar/>
-            <div className='p-4'>
-                <div className='bg-teal-200'>header</div>
+            <div className='flex-1'>
+                <Header/>
                 <div>{<Outlet/>}</div>
             </div>
         </div>
